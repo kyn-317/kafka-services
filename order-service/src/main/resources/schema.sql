1a9d5a67b957a95b39133769999139589000d8a6
kyn-317 <kynauto0317@gmail.com>
2025-05-07 21:56:07 +0900

    change schema

--- order-service/src/main/resources/schema.sql
+++ order-service/src/main/resources/schema.sql
@@ -2,6 +2,7 @@
 CREATE SCHEMA IF NOT EXISTS order_data;
 
 -- 테이블이 없는 경우 생성
+-- order by cart
 CREATE TABLE IF NOT EXISTS order_data.order (
     order_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     customer_id UUID NOT NULL,
@@ -13,6 +14,7 @@ CREATE TABLE IF NOT EXISTS order_data.order (
     updated_at TIMESTAMP
 );
 
+-- order default
 CREATE TABLE IF NOT EXISTS order_data.purchase_order (
     purchase_order_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     customer_id UUID NOT NULL,
@@ -29,6 +31,7 @@ CREATE TABLE IF NOT EXISTS order_data.purchase_order (
     updated_at TIMESTAMP
 );
 
+-- order detail
 CREATE TABLE IF NOT EXISTS order_data.order_detail (
     order_detail_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     order_id UUID NOT NULL REFERENCES order_data.order(order_id),
@@ -42,6 +45,7 @@ CREATE TABLE IF NOT EXISTS order_data.order_detail (
     updated_at TIMESTAMP
 );
 
+-- order workflow action
 CREATE TABLE IF NOT EXISTS order_data.order_workflow_action (
     order_workflow_action_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     order_id UUID NOT NULL REFERENCES order_data.order(order_id),
@@ -52,7 +56,7 @@ CREATE TABLE IF NOT EXISTS order_data.order_workflow_action (
     updated_at TIMESTAMP
 );
 
--- 테이블이 있다면 데이터 삭제 (외래 키를 가진 테이블부터 삭제)
+-- truncate table
 TRUNCATE TABLE order_data.order_workflow_action CASCADE;
 TRUNCATE TABLE order_data.order_detail CASCADE;
 TRUNCATE TABLE order_data.purchase_order CASCADE; 
